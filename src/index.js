0cda635591b364f33c0e1b73ca61fc57ecbb42bb
Santiago Moran <santimoran.1@hotmail.com>
2019-11-20 12:38:54 -0300

    Project setup and deploy in now

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Continuing with the advanced course</h1>, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const htmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    output: {
+        filename: 'app.bundle.js'
+    },
+    plugins: [
+        new htmlWebPackPlugin({
+            template: 'src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
