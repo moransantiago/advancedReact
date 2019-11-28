8207c92287d18453926b7dda9523ad3bd39a5eb9
Santiago Moran <santimoran.1@hotmail.com>
2019-11-28 17:50:11 -0300

    Custom hook LAZY LOADER created

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 
 import { PhotoCard } from '../PhotoCard/index'
 
-export const ListOfPhotoCards = () => [1, 2, 3].map(id => (<PhotoCard key={id} />))
+export const ListOfPhotoCards = () => [1, 2, 3, 4, 5, 6, 7, 8].map(id => (<PhotoCard key={id} />))
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,23 +1,42 @@
-import React from 'react'
+import React, { useEffect, useRef, useState, Fragment } from 'react'
 
-import { ImgWrapper, Img, Button } from './styles'
+import { Article, ImgWrapper, Img, Button } from './styles'
 
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
+  const [show, setShow] = useState(false)
+  const ref = useRef(null)
 
-      <Button>
-        <MdFavoriteBorder size='32px' />{likes} likes!
-      </Button>
-    </article>
+  useEffect(() => {
+    const observer = new window.IntersectionObserver(entries => {
+      const { isIntersecting } = entries[0]
+      if (isIntersecting) { //  <-- In case that the reference (in this case the article) is in the viewport, I render it
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+    observer.observe(ref.current)
+  }, [ref])
+
+  return (
+    <Article ref={ref}>
+      {
+        show &&
+          <Fragment>
+            <a href={`/detail/${id}`}>
+              <ImgWrapper>
+                <Img src={src} />
+              </ImgWrapper>
+            </a>
+
+            <Button>
+              <MdFavoriteBorder size='32px' />{likes} likes!
+            </Button>
+          </Fragment>
+      }
+    </Article>
   )
 }
