4b55b6cf413668047756fa8a67cc93bc2a0c2c4f
Santiago Moran <santimoran.1@hotmail.com>
2019-12-10 23:26:43 -0300

    Anchors changed to Links

--- src/components/Category/styles.js
+++ src/components/Category/styles.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
-export const Anchor = styled.a`
+import { Link as LinkRouter } from '@reach/router'
+
+export const Link = styled(LinkRouter)`
     display: flex;
     flex-direction: column;
     text-align: center;
--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -17,7 +17,7 @@ export const ImgWrapper = styled.div`
 `
 
 export const Img = styled.img`
-    ${fadeIn()}
+    ${fadeIn({ time: '0.5s' })}
     box-shadow: 0 10px 14px rgba(0, 0, 0, .2);
     height: 100%;
     object-fit: cover;
