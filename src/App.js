bb0e47911ee6f3627e4ca58dd3604d28704cedbb
Santiago Moran <santimoran.1@hotmail.com>
2019-12-17 15:22:05 -0300

    A few details corrected

--- src/App.js
+++ src/App.js
@@ -1,50 +1,38 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
-import { Router } from '@reach/router'
+import { Router, Redirect } from '@reach/router'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
+import { NotFound } from './pages/NotFound'
 
 import { Navbar } from './components/Navbar'
 import { Logo } from './components/Logo'
 import { GlobalStyle } from './components/styles/GlobalStyles'
 
-import Context from './Context'
+import { Context } from './Context'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return (
     <div>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path='/' />
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect from='/favs' to='/login' />}
+        {!isAuth && <Redirect from='/user' to='/login' />}
+        {isAuth && <Redirect from='/login' to='/' />}
+        <Favs path='/favs' />
+        <User path='/user' />
       </Router>
-      <Context.Consumer>
-        {
-          ({ isAuth }) => {
-            if (isAuth) {
-              return (
-                <Router>
-                  <Favs path='/favs' />
-                  <User path='/user' />
-                </Router>
-              )
-            } else {
-              return (
-                <Router>
-                  <NotRegisteredUser path='/favs' />
-                  <NotRegisteredUser path='/user' />
-                </Router>
-              )
-            }
-          }
-        }
-      </Context.Consumer>
       <Navbar />
     </div>
   )
---
+++ src/pages/NotFound.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export const NotFound = () => <h1>This page doesn't exist 😢</h1>
